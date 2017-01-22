5268abe6609030e508267557471f094990646eaa
Christina Lin <cristynalin@gmail.com>
2017-01-21 22:08:55 -0800

    Do Lab 1

--- handlers.js
+++ handlers.js
@@ -1,51 +1,91 @@
 // Toggles the style with name styleName on the tag, with either the value or
 // the default value.
 function toggleStyle(el, styleName, value) {
-  // TODO: if the value of the field corresponding to styleName on the
-  // element's style field is '', then set it to value. Otherwise, reset it to
-  // ''. e.g. if styleName = "color", we are trying to access the color field
-  // on the style field of element.
+  // If the value of the field corresponding to styleName on the element's 
+  // style field is '', then set it to value. Otherwise, reset it to ''.
+  if (el.style[styleName] === '') {
+    el.style[styleName] = value;
+  }
+  else {
+    // Assume value of styleName will not be set to any other values
+    el.style[styleName] = '';
+  }
 }
 
 function onFormSubmit(e) {
   var form = e.target;
-  // TODO: Prevent the form from actually submitting!  
-
-  // TODO: Print values of foo input and bar input together to response div.
+  // Prevent the form from actually submitting!  
+  e.preventDefault();
+  // Print values of foo input and bar input together to response div.
+  var form = document.getElementById("our_form");
+  var response = document.getElementById("response");
+  response.innerHTML = form.foo.value + " " + form.bar.value; // span tag
 }
 
 function formAlert(e) {
-  // TODO
+  e.preventDefault();
+  var form = document.getElementById("our_form");
+  alert("foo: " + form.foo.value + "\n" + "bar: " + form.bar.value);
 }
 
 function toggleBox(e) {
   var box = document.getElementById("box");
-  // TODO: Toggle the display of the box
+  // Toggle the visibility of the box
+  toggleStyle(box, "display", "none");
 }
 
 function rotateColors(e) {
   var box = document.getElementById("box");
-  // TODO: Rotate the colors of the box from red to blue to green.
+  // Rotate the colors of the box from red to blue to green.
+  if (box.style["background-color"].toLowerCase() === "red") {
+    box.style["background-color"] = "blue";
+  }
+  else if (box.style["background-color"].toLowerCase() === "blue") {
+    box.style["background-color"] = "green";
+  }
+  else {
+    box.style["background-color"] = "red";
+  }
 }
 
 function onTagButtonClick(e) {
   var el = e.target;
   var tagsEl = document.getElementById("tags");
-  // TODO: Check for the id here and determine which values to toggle. Then
-  // loop over all the tags you find and toggle the appropriate values.
+
+  var bTags;
+  var style;
+
+  if (el.id === "bold-btn") {
+    // Toggle bold words red
+    bTags = tagsEl.getElementsByTagName("b");
+    style = {name: "color", value: "red"};
+  }
+  else if (el.id === "italic-btn") {
+    // Toggle italic words' bg color grey
+    bTags = tagsEl.getElementsByTagName("i");
+    style = {name: "background-color", value: "gray"};
+  }
+  else if (el.id === "underline-btn") {
+    // Toggle underlined words' border 
+    bTags = tagsEl.getElementsByTagName("u");
+    style = {name: "border", value: "1px solid blue"};
+  }
+  else {
+    return;
+  }
+
+  for (var i = 0; i < bTags.length; i++) {
+    toggleStyle(bTags[i], style.name, style.value);
+  }
+
 }
 
 function initCanvas() {
   var c = document.getElementById("canvas");
   var ctx = c.getContext("2d");
-  // TODO: Fill the canvas with the color #ddd
-}
-
-function randomColor() {
-  var r = (Math.random() * 256 | 0).toString(16);
-  var g = (Math.random() * 256 | 0).toString(16);
-  var b = (Math.random() * 256 | 0).toString(16);
-  return "#" + r + g + b;
+  c.style["background-color"] = "#ddd";
+  ctx.clearRect(0, 0, c.width, c.height);
+  console.log("cleared");
 }
 
 function drawBox(e) {
@@ -60,7 +100,25 @@ function drawBox(e) {
   var mouseX = e.offsetX;
   var mouseY = e.offsetY;
   
-  // TODO: Fill a rectangle with a random color, with a width between 50 and
+  // Fill a rectangle with a random color, with a width between 50 and
   // 200 and a height between 50 and 100, such that it is centered around the
   // point (mouseX, mouseY)
+
+  var width = randomRange(50, 200);
+  var height = randomRange(50, 100);
+
+  ctx.fillStyle = randomColor();
+  ctx.fillRect(mouseX - (width / 2), mouseY - (height / 2), width, height);
+  ctx.beginPath();
+}
+
+function randomColor() {
+  var r = (Math.random() * 256 | 0).toString(16);
+  var g = (Math.random() * 256 | 0).toString(16);
+  var b = (Math.random() * 256 | 0).toString(16);
+  return "#" + r + g + b;
+}
+
+function randomRange(min, max) {
+  return Math.random() * (max - min + 1) + min;
 }

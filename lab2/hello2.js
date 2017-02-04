6301689fe2127d884e3978c435039218e0e6449c
Christina Lin <cristynalin@gmail.com>
2017-02-03 20:13:33 -0800

    Change hello2 to print five random greetings instead of a random greeting 5x

--- lab2/hello2.js
+++ lab2/hello2.js
@@ -17,8 +17,8 @@
     // prompt user for name asynchronously
     rl.question("What is your name? ", function(name) {
         // print random greeting five times
-        var index = Math.floor(Math.random() * greetings.length);
         for (var i = 0; i < 5; i++) {
+            var index = Math.floor(Math.random() * greetings.length);
             console.log(greetings[index] + ", " + name);
         }
         // and then close I/O interface

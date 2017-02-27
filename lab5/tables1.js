24da85d424346c958e6fbe401ba5202a6ebbb87d
Christina Lin <cristynalin@gmail.com>
2017-02-26 20:43:51 -0800

    Do lab 5

---
+++ lab5/tables1.js
@@ -0,0 +1,112 @@
+var cur_column = -1;
+var sort_dir = -1; // -1 is unsorted, 0 is ascending, 1 is descending
+
+function sortColumn(column) {
+  "use strict";
+  /*
+   * Sort the column given by the index, coloring the header as necessary.
+   *
+   * Some notes:
+   * - You will have to keep track of some sort of global state in order to know
+   *   which column you are currently sorting and which direction is the current
+   *   sort direction. This is kind of gross, but for this particular file you
+   *   can't really do much.
+   * - Arrays have a sort method that normally sorts by JavaScript's default
+   *   method, but you can actually provide your own function; if you provide a
+   *   function to Array.prototype.sort, it will take two arguments (comparing
+   *   items, say a and b), and return -1 if a < b, 1 if a > b, and 0 if a == b.
+   * - There's no really nice way to actually sort rows of things, so you'll
+   *   have to first store the rows in the correct order somewhere, clear out
+   *   the table, and append them back in the correct order.
+   */
+   var colors = ["blue", "red"];
+   var headers = document.getElementById("header").children;
+
+   if (column !== cur_column) {
+    // reset old column
+    if (cur_column >= 0) {
+      headers[cur_column].style.color = "";
+    }
+    // set variables for new column
+    cur_column = column;
+    sort_dir = 0;
+   }
+   else {
+    sort_dir = 1 - sort_dir; // toggle between 0 and 1
+   }
+
+   // update color
+   headers[cur_column].style.color = colors[sort_dir];
+
+   // get data from table
+   var trs = document.getElementsByTagName("tr");
+   var rows = [];
+   var tds;
+   var j;
+   var k;
+   for (j = 0; j < trs.length; j++){
+    if (trs[j].id !== "header") {
+      tds = trs[j].children;
+      var row = [];
+      for (k = 0; k < tds.length; k++) {
+        row.push(tds[k].innerHTML);
+      }
+      rows.push(row);
+    }
+    
+   }
+   
+   // sort
+   rows.sort(compareByCol);
+
+   // put data back in table
+   var skip = 0;
+   for (j = 0; j < trs.length; j++){
+    if (trs[j].id !== "header") {
+      tds = trs[j].children;
+      for (k = 0; k < tds.length; k++) {
+        tds[k].innerHTML = rows[j - skip][k];
+      }
+    }
+    else {
+      skip++;
+    }
+   }
+}
+
+function compareByCol(a, b) {
+  "use strict";
+  /* 
+   * Sort by cur_column and sort_dir.
+   */
+
+  if (a[cur_column] < b[cur_column]) {
+    if (sort_dir === 0) {
+      return -1;
+    }
+    return 1;
+  }
+  
+  if (a[cur_column] > b[cur_column]) {
+    if (sort_dir === 0) {
+      return 1;
+    }
+    return -1;
+  }
+
+  return 0;
+}
+
+window.addEventListener("load", function(e) {
+  "use strict";
+  // attach a click listener on all header cells
+  var i;
+  var elements = document.getElementById("header").children;
+  for (i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", (function(idx) {
+      return function(e) {
+        sortColumn(idx);
+      };
+    })(i));
+  }
+});
---
+++ lab5/tables2.js
@@ -0,0 +1,108 @@
+var SortTable = function(id, types) {
+  "use strict";
+  /*
+   * - Determine the types of each column based on the types argument; if it is
+   *   empty or not an array, just assume all the types are strings. Otherwise
+   *   push the type.
+   * - Set up the proper handlers (as before)
+   * - Set up any other variables you need (e.g. instance variables that would
+   *   have been globals before)
+   */
+
+  // declare instance variables
+  this.cur_column = -1;
+  this.sort_dir = -1; // -1 is unsorted, 0 is ascending, 1 is descending
+  this.colors = ["blue", "red"];
+
+  // attach listeners to headers
+  var headers = document.getElementById(id).getElementsByTagName("thead")[0];
+  this.header_elements = headers.children[0].children; // first row of header
+
+  var i;
+  for (i = 0; i < this.header_elements.length; i++) {
+    this.header_elements[i].addEventListener("click", (function(idx) {
+      return function(e) {
+        this.sortColumn(idx);
+      };
+    })(i).bind(this));
+  }
+
+  // read data from the table and set correct type
+  this.rows = [];
+  var trs = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+  var tds;
+  var j;
+  var k;
+  for (j = 0; j < trs.length; j++){
+  tds = trs[j].children;
+  var row = [];
+  for (k = 0; k < tds.length; k++) {
+    if (types[k] === Number) {
+      row.push(Number(tds[k].innerHTML));
+    }
+    else { // default is String
+      row.push(tds[k].innerHTML); 
+    } 
+  }
+  this.rows.push(row);
+  }
+};
+
+SortTable.prototype.sortColumn = function(index) {
+  "use strict";
+  /* 
+   * Sort by cur_column and sort_dir.
+   */
+
+   if (index !== this.cur_column) {
+    // reset old column
+    if (this.cur_column >= 0) {
+      this.header_elements[this.cur_column].style.color = "";
+    }
+    // set variables for new column
+    this.cur_column = index;
+    this.sort_dir = 0;
+   }
+   else {
+    this.sort_dir = 1 - this.sort_dir; // toggle between 0 and 1
+   }
+
+   // update color
+   this.header_elements[this.cur_column].style.color = this.colors[this.sort_dir];
+   
+   // sort
+   this.rows.sort(this.compareByCol.bind(this));
+
+   // put data back in table
+   var trs = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+   var tds;
+   var j;
+   var k;
+   for (j = 0; j < trs.length; j++){
+    tds = trs[j].children;
+    for (k = 0; k < tds.length; k++) {
+      tds[k].innerHTML = this.rows[j][k];
+    }
+   }
+};
+
+SortTable.prototype.compareByCol = function(a, b) {
+  "use strict";
+  /* Sort by cur_column and sort_dir */
+  if (a[this.cur_column] < b[this.cur_column]) {
+    if (this.sort_dir === 0) {
+      return -1;
+    }
+    return 1;
+  }
+  
+  if (a[this.cur_column] > b[this.cur_column]) {
+    if (this.sort_dir === 0) {
+      return 1;
+    }
+    return -1;
+  }
+
+  return 0;
+
+};

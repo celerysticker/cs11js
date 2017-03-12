ad79c1eb02db78ce50d794a173b9d97a82ab1903
Christina Lin <cristynalin@gmail.com>
2017-03-12 00:19:10 -0800

    Do lab 6

--- lab6/client.js
+++ lab6/client.js
@@ -8,39 +8,76 @@ function ChatClient(port) {
   });
 
   // this.client is a socket connecting the client with the server
-  this.client = /* TODO: Connect to the server on its port */;
+  this.client = net.connect(4242); /* Connect to the server on its port */
   this.clientId = null;
 
   this.attachIOListeners();
   this.attachChatListeners();
-};
+}
 
 ChatClient.prototype.attachIOListeners = function() {
-  this.io.on("line", /* TODO: Handle user input */);
+  this.io.on("line", function(line) {
+    this.client.write(line);
+  }.bind(this));
 
   this.io.on("SIGINT", function() {
     console.log("Closing client...");
 
-    // TODO: Close client connection and I/O listener
-    // Hint: Make sure to set things to null!
+    // Close client connection and I/O listener
+    this.io.close();
+    this.client.end();
   }.bind(this));
 };
 
 ChatClient.prototype.attachChatListeners = function() {
   // TODO: Handle JSON data when the client socket emits the "data" event
+  this.client.on("data", function(msg) {
+    var msgs = msg.toString().split("\n");
+    for (var m in msgs) {
+      if (msgs[m].length > 0) {
+        var data = JSON.parse(msgs[m]);
+        if (data.type === "OK") {
+          this.clientId = data.id;
+        }
+
+        if (data.type === "JOIN") {
+          console.log("Client #" + data.clientId + " has joined.");
+        }
+
+        if (data.type === "MSG") {
+          if (parseInt(data.clientId) === parseInt(this.clientId)) {
+            console.log("ME: " + data.message);
+          }
+          else {
+            console.log("CLIENT #" + data.clientId + ": " + data.message);
+          }
+        }
+
+        if (data.type === "LEAVE") {
+          console.log("Client #" + data.clientId + " has left.");
+        }
+      }
+    }
+  }.bind(this));
 
   this.client.on("end", function() {
     if(this.client) {
-      // TODO: Handle a client exiting
+      // Handle a client exiting
       // If the client exists, that means the server closed the connection, so
       // we have to make sure we remove the client!
+      console.log("Server closed connection.");
+      this.io.close();
+      this.client.end();
     }
   }.bind(this));
 
   this.client.on("error", function(e) {
-    // TODO: Handle errors
+    // Handle errors
     // Hint: This happens only when something is wrong with the I/O or the
     // client, so close their connections.
+    console.log("Error: " + e);
+    this.io.close();
+    this.client.end();
   }.bind(this));
 };
 
--- lab6/server.js
+++ lab6/server.js
@@ -8,33 +8,71 @@ function ChatServer(port) {
 
   this.attachListeners();
 
-  this.server.listen(/* TODO: Complete this function */);
+  this.server.listen(port, function() {
+    console.log("Server listening on port " + String(port));
+  }.bind(this));
 
 }
 
 // Broadcast the same message to each connected client.
-ChatServer.prototype.broadcast = function(message) {
-  // TODO: Complete this function
+ChatServer.prototype.broadcast = function(data) {
+  for (var key in this.clients) {
+    this.clients[key].write(JSON.stringify(data) + "\n");
+  }
 };
 
 // This function is called whenever a new client connection is made.
 ChatServer.prototype.onConnection = function(socket) {
-  // TODO: Increment IDs
+  // Increment IDs
+  this.totalClients++;
+  var id = this.totalClients;
 
-  // TODO: Log which client connected and send OK to client
+  // Log which client connected and send OK to client
+  console.log("Client #" + String(id) + " connected!");
+  var welcome = {
+    "type": "OK",
+    "id": id
+  };
+  socket.write(JSON.stringify(welcome) + "\n");
 
-  // TODO: Use broadcast function to send a join JSON message
+  // Use broadcast function to send a join JSON message
+  var announce = {
+    "type": "JOIN",
+    "clientId": id
+  };
+  this.broadcast(announce);
 
-  // TODO: Broadcast msg message when "data" event comes from client
+  // Broadcast msg message when "data" event comes from client
+  socket.on("data", function(msg) {
+    console.log("RECV(" + id + "): " + msg);
+    var data = {
+      "type": "MSG",
+      "clientId": id,
+      "message": msg.toString()
+    };
+    this.broadcast(data);
+  }.bind(this));
 
-  // TODO: Handle client closing when "close" event comes from client
+  // Handle client closing when "close" event comes from client
+  socket.on("close", function(is_error) {
+    delete this.clients[id];
+    console.log("Client #" + id + " closed its connection.");
+    var leave = {
+      "type": "LEAVE",
+      "clientId": id
+    };
+    this.broadcast(leave);
+  }.bind(this));
 
-  // TODO: Add client to clients map
+  // Add client to clients map
+  this.clients[id] = socket;
+
 };
 
 // Attach listeners for "connection" and "error" events.
 ChatServer.prototype.attachListeners = function() {
-  // TODO: Handle connection and error events
+  // Handle connection and error events
+  this.server.on("connection", this.onConnection.bind(this));
 };
 
 var server = new ChatServer(4242);
